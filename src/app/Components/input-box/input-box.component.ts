b2e6fb797530c5002fc660d513e4ba4c2ba0b108
thangluu <luuthang9300@gmail.com>
2022-01-02 20:02:51 +0700

    Change

--- src/app/Components/input-box/input-box.component.ts
+++ src/app/Components/input-box/input-box.component.ts
@@ -1,4 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, ContentChild, ElementRef, OnInit, ViewChild} from '@angular/core';
+import {debounce} from 'lodash'
+import {BehaviorSubject, Observable, of, Subject} from "rxjs";
+import {HttpClient} from "@angular/common/http";
+import {environment} from "../../../environments/environment";
+import {switchMap} from "rxjs/operators";
 
 @Component({
   selector: 'app-input-box',
@@ -6,27 +11,63 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./input-box.component.scss']
 })
 export class InputBoxComponent implements OnInit {
-  
-  constructor() { 
-    
-  }
-  values = '';
-  words = '';
-  removespace= '';
-  characters= '';
+
+  constructor(private http: HttpClient) {}
+
+  wordSubject: BehaviorSubject<any> = new BehaviorSubject([]);
+  words: Observable<any> = of([]);
   showRightIcon: boolean = false;
+  inputValue: string = 'hello';
 
   ngOnInit(): void {
+    let words = this.inputValue.split(' ')
+    let wordFormatted = [];
+    for (let word of words) {
+      wordFormatted.push({
+        text: word,
+        error: 0
+      })
+    }
+    this.wordSubject.next(wordFormatted);
+
   }
- 
-  onKey(value: string) {
-    // count words in the value
-      this.words = value.split(' ').length.toString();
-      this.removespace = value.replace(/\s/g, '');
-      this.characters = this.removespace.length.toString();
-      if (value.length > 0) {
-        this.showRightIcon = true;
+
+  onKey() {
+    this.parseStringToArray(this.inputValue)
+  }
+
+  checkValidInput() {
+    console.log(123)
+    this.http.post(environment.Endpoint, {
+      entries: this.wordSubject.value
+    }).toPromise().then((res: any) => {
+      console.log(res)
+      this.wordSubject.next(res.entries || [])
+    })
+  }
+
+  deleteInput() {
+    this.inputValue = this.inputValue.slice(0, -1);
+    this.parseStringToArray(this.inputValue)
+  }
+
+  parseStringToArray(data: string) {
+    if (data) {
+
+      let words = data.split(' ');
+      let wordsFormatted = [];
+      for (let work of words) {
+        wordsFormatted.push({text: work, error: 0});
       }
+      this.wordSubject.next(wordsFormatted);
+      // if (pTag?.innerText) {
+      //   pTag.innerText = '';
+      // }
+    }
+  }
+
+  get WordObservable(): Observable<any> {
+    return this.wordSubject.asObservable()
   }
 
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,27 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { HeaderComponent } from './Components/header/header.component';
-import { InputBoxComponent } from './Components/input-box/input-box.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {HeaderComponent} from './Components/header/header.component';
+import {InputBoxComponent} from './Components/input-box/input-box.component';
+import {FormsModule} from "@angular/forms";
+import {HttpClientModule} from "@angular/common/http";
+
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     InputBoxComponent
-    
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
